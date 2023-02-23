f78189c5d03b266f55df3eab5376ff257f7ceb25
Elena <Elenakhot@mail.ru>
2023-02-23 14:03:25 +0300

    commit fournewone

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -5,17 +5,15 @@ public class Radio {
     public void next() {
         if (currentStation != 9) {
             currentStation++;
-        } else {
-            currentStation = 0;
         }
+            currentStation = 0;
     }
 
     public void prev() {
         if (currentStation != 0) {
             currentStation--;
-        } else {
-            currentStation = 9;
         }
+            currentStation = 9;
     }
 
     public void maxVol() {
