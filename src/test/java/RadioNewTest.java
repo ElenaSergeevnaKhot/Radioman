57f1a55c388975c91171256527de2edbb365a6ab
Elena <Elenakhot@mail.ru>
2023-02-26 15:10:28 +0300

    commit

---
+++ src/test/java/RadioNewTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RadioNewTest {
+
+    @Test
+    public void test() {
+        Radio radio = new Radio(29);
+
+        radio.setCurrentStation(15);
+
+        int expected = 15;
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+}
+}
