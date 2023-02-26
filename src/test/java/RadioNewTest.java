017695a6ea5edb9bbd2f5d4135f33346d09cf070
Elena <Elenakhot@mail.ru>
2023-02-26 16:44:23 +0300

    commit

--- src/test/java/RadioNewTest.java
+++ src/test/java/RadioNewTest.java
@@ -13,4 +13,30 @@ public class RadioNewTest {
         int actual = radio.getCurrentStation();
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    public void test40() {
+        Radio radio = new Radio(29);
+
+        radio.setCurrentStation(29);
+
+        int expected = 0;
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test42() {
+        Radio radio = new Radio(30);
+
+        radio.setCurrentStation(30);
+
+        int expected = 0;
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+
 }
+
+
